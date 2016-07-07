2a4a4ead8d54604ed8acf12322c7c8f2d8bcb13c
AntonPavl <pavlenok.anton@gmail.com>
2016-07-07 16:42:56 +0300

    Initial

---
+++ Da4/Task1_JaggedArraySorter/JaggedArraySorter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_JaggedArraySorter
+{
+    public class JaggedArraySorter
+    {
+        /// <summary>
+        /// SUMSSORT = sort by the sum of the elements in array[]
+        /// MAXELEMENTSSORT = sort by the maximum element in array[]
+        /// MINELEMENTSSORT = sort by the minimum element in array[]
+        /// </summary>
+        public enum SortType
+        {
+            SUMSORT,
+            MAXELEMENTSSORT,
+            MINELEMENTSSORT
+        }
+
+        private static Dictionary<SortType, Func<double[], double[], bool, bool>> sortNavigator =
+            new Dictionary<SortType, Func<double[], double[], bool, bool>>()
+        {
+                {
+                    SortType.SUMSORT,
+                    (a,b,inverse) => ( inverse ? a.Sum()<b.Sum() : a.Sum()>b.Sum())
+                },
+                 {
+                    SortType.MAXELEMENTSSORT,
+                    (a,b,inverse) => ( inverse ? a.Max()<b.Max() : a.Max()>b.Max())
+                },
+                 {
+                    SortType.MINELEMENTSSORT,
+                    (a,b,inverse) => ( inverse ? a.Min()<b.Min() : a.Min()>b.Min())
+                }
+        };
+
+        /// <summary>
+        /// Sort arrays in array by the Comparator from enum
+        /// </summary>
+        /// <param name="array">Array[][] with values</param>
+        /// <param name="sortFun">Comparator from enum</param>
+        /// <param name="inverse">true - Descending, false - Ascending</param>
+        public static void Sorter(double[][] array, SortType sortFun= SortType.SUMSORT, bool inverse = false)
+        {
+            if (array == null) throw new ArgumentNullException();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == null) throw new ArgumentNullException();
+            }
+            Sort(array, sortNavigator[sortFun], inverse);
+        }
+
+        /// <summary>
+        /// Sort arrays in array by the Comparator
+        /// </summary>
+        /// <param name="array">Array[][] with values</param>
+        /// <param name="sortFun">Comparator</param>
+        /// <param name="inverse">true - Descending, false - Ascending</param>
+        public static void Sorter(double[][] array, Func<double[], double[], bool> sortFun)
+        {
+            Sort(array, sortFun);
+        }
+
+        private static void Sort(double[][] array,Func<double[],double[],bool,bool> compare,bool inverse)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (compare(array[j],array[j+1],inverse))
+                    {
+                        double[] temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        private static void Sort(double[][] array, Func<double[], double[], bool> compare)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (compare(array[j], array[j + 1]))
+                    {
+                        double[] temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Da4/Task1_JaggedArraySorterNTests/JaggedArraySorterNTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using NUnit.Framework;
+using Task1_JaggedArraySorter;
+namespace Task1_JaggedArraySorterNTests
+{
+    [TestFixture]
+    public class JaggedArraySorterNTests
+    {
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortSum_ToUp()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100,200,300};
+            arr[1] = new double[] { 10,20};
+            arr[2] = new double[] { 1};
+
+            JaggedArraySorter.Sorter(arr,JaggedArraySorter.SortType.SUMSORT);
+
+            Assert.IsTrue(arr[0][0] == 1 && arr[1][0] == 10 && arr[2][0] == 100);
+        }
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortSum_ToDown()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorter.Sorter(arr,JaggedArraySorter.SortType.SUMSORT,inverse:true);
+            Assert.IsTrue(arr[0][0] == 100 && arr[1][0] == 10 && arr[2][0] == 1);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMax_ToDown()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorter.Sorter(arr,JaggedArraySorter.SortType.MAXELEMENTSSORT, inverse: true);
+            Assert.IsTrue(arr[0][0] == 100 && arr[1][0] == 10 && arr[2][0] == 1);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMax_ToUp()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MAXELEMENTSSORT);
+            Assert.IsTrue(arr[0][0] == 1 && arr[1][0] == 10 && arr[2][0] == 100);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMin_ToDown()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MINELEMENTSSORT, inverse: true);
+            Assert.IsTrue(arr[0][0] == 100 && arr[1][0] == 10 && arr[2][0] == 1);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMin_ToUp()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MINELEMENTSSORT);
+            Assert.IsTrue(arr[0][0] == 1 && arr[1][0] == 10 && arr[2][0] == 100);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void JaggedArraySorter_ArrayD3_ArrayNull_ArgumentNullExep()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = null;
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MINELEMENTSSORT);
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void JaggedArraySorter_ArrayD3Null_ArgumentNullExep()
+        {
+            double[][] arr = null;
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MINELEMENTSSORT);
+
+        }
+    }
+}
