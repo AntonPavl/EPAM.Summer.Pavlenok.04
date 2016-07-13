e601c9b13a02ee4fb3060ad10abc2b3df8f35cb5
AntonPavl <pavlenok.anton@gmail.com>
2016-07-13 14:59:25 +0300

    Add Interfaces

---
+++ Da4/Task1_JaggedArraySorter/JaggedArraySorterInterfaces.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_JaggedArraySorter
+{
+    public static class JaggedArraySorterInterfaces
+    {
+
+        /// <summary>
+        /// Sort arrays in array by the Comparator
+        /// </summary>
+        /// <param name="array">Array[][] with values</param>
+        /// <param name="sortFun">Comparator</param>
+        /// <param name="inverse">DES - Descending, ASC - Ascending</param>
+        public static void Sorter(double[][] array, IComparer<double[]> comparator)
+        {
+            if (array == null) throw new ArgumentNullException();
+            if (array.Length == 0) return;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == null) throw new ArgumentNullException();
+            }
+
+            Sort(array, comparator);
+        }
+
+        private static void Sort(double[][] array, IComparer<double[]> comparator)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (comparator.Compare(array[j], array[j + 1]) > 0)
+                        Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+
+        private static void Swap(ref double[] a, ref double[] b)
+        {
+            double[] temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
---
+++ Da4/Task1_JaggedArraySorterNTests/JaggedARraySorterInterNTest.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using NUnit.Framework;
+using Task1_JaggedArraySorter;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1_JaggedArraySorterNTests
+{
+    [TestFixture]
+    public class JaggedARraySorterInterNTest
+    {
+        public class SumAsc : IComparer<double[]>
+        {
+            public int Compare(double[] x, double[] y)
+            {
+                if (x.Sum() > y.Sum()) return 1;
+                if (x.Sum() == y.Sum()) return 0;
+                else return -1;
+            }
+        }
+        public class SumDesc : IComparer<double[]>
+        {
+            public int Compare(double[] x, double[] y)
+            {
+                if (x.Sum() < y.Sum()) return 1;
+                if (x.Sum() == y.Sum()) return 0;
+                else return -1;
+            }
+        }
+        public class MaxAsc : IComparer<double[]>
+        {
+            public int Compare(double[] x, double[] y)
+            {
+                if (x.Max() > y.Max()) return 1;
+                if (x.Max() == y.Max()) return 0;
+                else return -1;
+            }
+        }
+        public class MaxDesc : IComparer<double[]>
+        {
+            public int Compare(double[] x, double[] y)
+            {
+                if (x.Max() < y.Max()) return 1;
+                if (x.Max() == y.Max()) return 0;
+                else return -1;
+            }
+        }
+        public class MinAsc : IComparer<double[]>
+        {
+            public int Compare(double[] x, double[] y)
+            {
+                if (x.Min() > y.Min()) return 1;
+                if (x.Min() == y.Min()) return 0;
+                else return -1;
+            }
+        }
+        public class MinDesc : IComparer<double[]>
+        {
+            public int Compare(double[] x, double[] y)
+            {
+                if (x.Min() < y.Min()) return 1;
+                if (x.Min() == y.Min()) return 0;
+                else return -1;
+            }
+        }
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortSum_ToUp()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+            JaggedArraySorterInterfaces.Sorter(arr,new SumAsc());
+            Assert.IsTrue(arr[0][0] == 1 && arr[1][0] == 10 && arr[2][0] == 100);
+        }
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortSum_ToDown()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+            JaggedArraySorterInterfaces.Sorter(arr, new SumDesc());
+            Assert.IsTrue(arr[0][0] == 100 && arr[1][0] == 10 && arr[2][0] == 1);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMax_ToDown()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+            JaggedArraySorterInterfaces.Sorter(arr, new MaxDesc());
+            Assert.IsTrue(arr[0][0] == 100 && arr[1][0] == 10 && arr[2][0] == 1);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMax_ToUp()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorterInterfaces.Sorter(arr, new MaxAsc());
+            Assert.IsTrue(arr[0][0] == 1 && arr[1][0] == 10 && arr[2][0] == 100);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMin_ToDown()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorterInterfaces.Sorter(arr, new MinDesc());
+            Assert.IsTrue(arr[0][0] == 100 && arr[1][0] == 10 && arr[2][0] == 1);
+        }
+
+        [Test]
+        public void JaggedArraySorter_ArrayD3_SortMin_ToUp()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = new double[] { 1 };
+
+            JaggedArraySorterInterfaces.Sorter(arr, new MinAsc());
+            Assert.IsTrue(arr[0][0] == 1 && arr[1][0] == 10 && arr[2][0] == 100);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void JaggedArraySorter_ArrayD3_ArrayNull_ArgumentNullExep()
+        {
+            double[][] arr = new double[3][];
+            arr[0] = new double[] { 100, 200, 300 };
+            arr[1] = new double[] { 10, 20 };
+            arr[2] = null;
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MIN);
+            JaggedArraySorterInterfaces.Sorter(arr, new MinDesc());
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void JaggedArraySorter_ArrayD3Null_ArgumentNullExep()
+        {
+            double[][] arr = null;
+            JaggedArraySorter.Sorter(arr, JaggedArraySorter.SortType.MIN);
+
+        }
+    }
+}
