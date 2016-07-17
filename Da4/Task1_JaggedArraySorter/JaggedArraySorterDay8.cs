69b4a2df56482f8860da96dd8549c59b1a0a00f0
AntonPavl <pavlenok.anton@gmail.com>
2016-07-17 13:56:57 +0300

    Adapter

--- Da4/Task1_JaggedArraySorter/JaggedArraySorterDay8.cs
+++ Da4/Task1_JaggedArraySorter/JaggedArraySorterDay8.cs
@@ -8,17 +8,27 @@ namespace Task1_JaggedArraySorter
 {
     public static class JaggedArraySorterDay8
     {
+        private class Adapter : IComparer<double[]>
+        {
+            public Func<double[],double[],int> Comparer { get; }
+            public Adapter(Func<double[],double[],int> comparer)
+            {
+                Comparer = comparer;
+            }
+            public int Compare(double[] a, double[] b)
+            {
+                return Comparer(a, b);
+            }
+        }
+
         /// <summary>
-        /// Sort arrays in array by the DelegateInInterfae comparator
+        /// Sort arrays in array by the delegate comparator
         /// </summary>
         /// <param name="array">Array[][] with values</param>
-        /// <param name="delegate">delegate in interface</param>
-        /// <param name="comparator">Comparator</param>
-        public static void Sorter(double[][] array, Func<double[],double[],int> @delegate, IDelegateComparer comparator)
+        /// <param name="delegate">comparator </param>
+        public static void SorterDelegate(double[][] array, Func<double[], double[], int> @delegate)
         {
-            if (ReferenceEquals(array, null) ||
-                ReferenceEquals(@delegate, null) ||
-                ReferenceEquals(comparator, null))
+            if (ReferenceEquals(array, null) || ReferenceEquals(@delegate, null))
                 throw new ArgumentNullException();
 
             if (array.Length == 0) return;
@@ -27,54 +37,70 @@ namespace Task1_JaggedArraySorter
             {
                 if (ReferenceEquals(array[i], null)) throw new ArgumentNullException();
             }
-            Sort(array, @delegate,comparator);
+            Sort(array, @delegate);
         }
 
-        private static void Sort(double[][] array, Func<double[], double[], int> @delegate, IDelegateComparer comparator)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length - 1; j++)
-                {
-                    if (comparator.Compare(array[j], array[j + 1],@delegate) > 0)
-                        Swap(ref array[j], ref array[j + 1]);
-                }
-            }
-        }
 
         /// <summary>
-        /// Sort arrays in array by the InterfaceInDelegate comparator
+        /// Sort arrays in array by the Interface to delegate comparator
         /// </summary>
         /// <param name="array">Array[][] with values</param>
-        /// <param name="delegate">delegate in interface</param>
         /// <param name="comparator">Comparator</param>
-        public static void Sorter(double[][] array, Func<double[], double[], IDelegateComparer, int> @delegate, IDelegateComparer comparator)
+        public static void SorterDelegate(double[][] array, IComparer<double[]> comparator) =>
+            SorterDelegate(array, comparator.Compare);
+
+
+        /// <summary>
+        /// Sort arrays in array by the delegate to interface comparator
+        /// </summary>
+        /// <param name="array">Array[][] with values</param>
+        /// <param name="delegate">comparator</param>
+        public static void SorterInterf(double[][] array, Func<double[], double[], int> @delegate) =>
+            SorterInterf(array, new Adapter(@delegate));
+
+
+        /// <summary>
+        /// Sort arrays in array by the interface comparator
+        /// </summary>
+        /// <param name="array">Array[][] with values</param>
+        /// <param name="comparator">Comparator</param>
+        public static void SorterInterf(double[][] array, IComparer<double[]> comparator)
         {
-            if (ReferenceEquals(array, null)     ||
-                ReferenceEquals(@delegate, null) || 
-                ReferenceEquals(comparator, null)) throw new ArgumentNullException();
+            if (ReferenceEquals(array, null) ||
+                  ReferenceEquals(comparator, null))
+                throw new ArgumentNullException();
 
             if (array.Length == 0) return;
 
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] == null) throw new ArgumentNullException();
+                if (ReferenceEquals(array[i], null)) throw new ArgumentNullException();
             }
-            Sort(array, @delegate, comparator);
+            Sort(array,comparator);
         }
 
-        private static void Sort(double[][] array, Func<double[],double[],IDelegateComparer, int> @delegate, IDelegateComparer comparator)
+        private static void Sort(double[][] array, Func<double[],double[],int> @delegate)
         {
             for (int i = 0; i < array.Length; i++)
             {
                 for (int j = 0; j < array.Length - 1; j++)
                 {
-                    if (@delegate(array[j], array[j + 1], comparator) > 0)
+                    if (@delegate(array[j], array[j + 1]) > 0)
                         Swap(ref array[j], ref array[j + 1]);
                 }
             }
         }
-
+        private static void Sort(double[][] array, IComparer<double[]> comparer)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (comparer.Compare(array[j], array[j + 1]) > 0)
+                        Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
 
         private static void Swap(ref double[] a, ref double[] b)
         {
